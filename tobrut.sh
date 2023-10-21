19418a12013961a5c3a9899d053ce71ce57ded1d
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-10-22 02:09:27 +0700

    kentot lu

--- tobrut.sh
+++ tobrut.sh
@@ -54,18 +54,18 @@ timedatectl set-timezone Asia/Jakarta;
 sudo bash -c "$(curl -sL https://github.com/GawrAme/Marzban-scripts/raw/master/marzban.sh)" @ install
 
 #install env
-wget -O /opt/marzban/.env "https://raw.githubusercontent.com/GawrAme/MarLing/main/env"
+wget -O /opt/marzban/.env "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/env"
 
 #profile
 echo -e 'profile' >> /root/.profile
-wget -O /usr/bin/profile "https://raw.githubusercontent.com/GawrAme/MarLing/main/profile";
+wget -O /usr/bin/profile "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/profile";
 chmod +x /usr/bin/profile
 apt install neofetch -y
-wget -O /usr/bin/cekservice "https://raw.githubusercontent.com/GawrAme/MarLing/main/cekservice.sh"
+wget -O /usr/bin/cekservice "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/cekservice.sh"
 chmod +x /usr/bin/cekservice
 
 #install compose
-wget -O /opt/marzban/docker-compose.yml "https://raw.githubusercontent.com/GawrAme/MarLing/main/docker-compose.yml"
+wget -O /opt/marzban/docker-compose.yml "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/docker-compose.yml"
 
 #Install VNSTAT
 apt -y install vnstat
@@ -89,9 +89,9 @@ sudo apt-get install speedtest -y
 #install nginx
 apt install nginx -y
 rm /etc/nginx/conf.d/default.conf
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/GawrAme/MarLing/main/nginx.conf"
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/GawrAme/MarLing/main/vps.conf"
-wget -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/GawrAme/MarLing/main/xray.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/nginx.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/vps.conf"
+wget -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/xray.conf"
 systemctl enable nginx
 mkdir -p /var/www/html
 echo "<pre>Setup by LingVPN</pre>" > /var/www/html/index.html
@@ -108,7 +108,7 @@ curl https://get.acme.sh | sh -s email=akunpispon2@gmail.com
 /root/.acme.sh/acme.sh --server letsencrypt --register-account -m akunpispon2@gmail.com --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /var/lib/marzban/xray.crt --keypath /var/lib/marzban/xray.key --ecc
 systemctl start nginx
-wget -O /var/lib/marzban/xray_config.json "https://raw.githubusercontent.com/GawrAme/MarLing/main/xray_config.json"
+wget -O /var/lib/marzban/xray_config.json "https://raw.githubusercontent.com/lunoxxdev/tT0OBrRuTT/main/xray_config.json"
 
 #install firewall
 apt install ufw -y
